e33d357dbcfd7e050bf17f90d81d9cebff434122
kwang929 <kevin.gm.wang@gmail.com>
2022-11-01 13:09:11 -0700

    userLogin file completed but not yet functional

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, createContext, UserContext } from 'react';
 import { render } from 'react-dom';
+import { RouteContext } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard';
+import UserLogin from './components/userLogin';
+import UserProfile from './components/userProfile';
+import List from './components/list';
 
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  
   return (
-    <div>
-      hi bitches do you see my steamy ass text
+    <div id='app'>
+      <UserContext.Provider value = {[user, setUser]}>
+        <Routes>
+          <Route index element={<Dashboard />} />
+          <Route index path='/*' element={<UserLogin />} />
+          <Route index path='/*' element={<UserProfile />} />
+          <Route index path='/*' element={<List />} />
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
 
 render(<App />, document.querySelector('#root'));
+
+export { App, UserContext };
